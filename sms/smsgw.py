7fc6b099875f544c0f6173e26a7fa71745aaf45f
Michal Ludvig <mludvig@logix.net.nz>
2012-03-30 23:18:19 +1300

    Fix passing phone number to sms-cli

--- sms/smsgw.py
+++ sms/smsgw.py
@@ -14,13 +14,16 @@ cfg = Config(settings.SMS_CLI_CONF, settings.SMS_CLI_PROFILE)
 
 def despatch(message, contact):
 	sender = SmsSender()
-	sms_id = sender.send(message.sms_body, contact.sms_number)
-	return sms_id, sender.get_status(sms_id)
+	sms_id = sender.send(message.sms_body, [contact.sms_number])
+	return sms_id, get_status(sms_id)
 
 def get_status(sms_id):
 	"""
 	get_status(sms_id) -> returns Sms.Sender.SmsStatus() object
 	"""
 	sender = SmsSender()
-	status = sender.get_status(sms_id)
+	try:
+		status = sender.get_status(sms_id)
+	except AttributeError, e:
+		return "UNKNOWN"
 	return status
