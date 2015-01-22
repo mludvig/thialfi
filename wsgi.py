11b1a4675de5612d223d81c8fcde687a8365d505
Michael Ludvig <mludvig@logix.net.nz>
2015-01-22 18:34:50 +1300

    Fix wsgi.py to work with thialfi-docker git rev 93415801e and newer

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,8 @@
 import os, sys
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
+sys.path.append(os.path.dirname(__file__))
+
 import django.core.handlers.wsgi
 
 application = django.core.handlers.wsgi.WSGIHandler()
