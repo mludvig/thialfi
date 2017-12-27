2d86ca5eb4293fda0fcc8de57624905dd489e8d0
Michael Ludvig <mludvig@logix.net.nz>
2017-12-27 18:34:40 +1300

    Update to twilio==6.1 compatibility

--- voice/twiliogw.py
+++ voice/twiliogw.py
@@ -1,10 +1,10 @@
 import sys
 from thialfi import settings
 from thialfi.logger import *
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 
 __all__ = []
-client = TwilioRestClient(settings.TWILIO_SID, settings.TWILIO_TOKEN)
+client = Client(settings.TWILIO_SID, settings.TWILIO_TOKEN)
 
 __all__.append('make_call')
 def make_call(to, url):
@@ -20,6 +20,3 @@ def list_calls(**kwargs):
     for call in calls:
         debug("call: %s %s %s %s %s %s" % (call.start_time, call.duration, call.sid, call.from_, call.to, call.status))
     return calls
-
-if __name__ == "__main__":
-    make_call("+64210742741", "http://tmp.logix.cz/voice.xml")
